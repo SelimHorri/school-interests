44109ffd89d10eea85fef91d877ed1c4fb8f193b
SelimHorri <horri.slim@gmail.com>
2021-02-21 18:25:21 +0100

    remove V1 and V2 from root dir

--- backend/V4__insert_students_table.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO students (first_name, last_name, email, phone, school_id) VALUES
-('Selim', 'Horri', 'slim__horri@hotmail.com', '22125144', 2),
-('Amine', 'Ladjimi', 'amine.ladjimi@gmail.com', '22125144', 1),
-('Omar', 'Derouiche', 'omar.derouiche@gmail.com', '22125144', 1);
-
